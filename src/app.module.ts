5d1dfa84b2dd157ff375c773e14507fb414fc56d
reasonly8 <reasonly8@sina.com>
2024-10-17 17:28:44 +0800

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { importConfigModule } from './config/importConfigModule';
 import { AppController } from './app.controller';
+import { importTypeOrmModule } from './typeorm/importTypeOrmModule';
 
 @Module({
-  imports: [importConfigModule()],
+  imports: [importConfigModule(), importTypeOrmModule()],
   controllers: [AppController],
   providers: [],
 })
